066c71f0a7762b4444ea9cf2d0fccea8c802d744
Bruno Righi <102252078+BrunoozL@users.noreply.github.com>
2022-09-21 15:28:51 -0300

    Add files via upload

---
+++ inserts.sql
@@ -0,0 +1,33 @@
+-- INSERT DOS PRODUTOS
+
+insert into produto (descricao, preco, estoque)
+values ("Camisa", "30","20");
+
+insert into produto (descricao, preco, estoque)
+values ("Cueca", "10","40");
+
+insert into produto (descricao, preco, estoque)
+values ("Calça", 35, 10);
+
+insert into produto (descricao, preco, estoque)
+values ("Tenis", 60, 25);
+
+insert into produto (descricao, preco, estoque)
+values ("Meia", 5, 50);
+
+insert into produto (descricao, preco, estoque)	
+values ("Blusa", 50, 8);
+
+-- INSERT DAS VENDAS
+
+insert into venda (cliente, data_venda)
+values ("Roberto", "2022-09-13 14:42:58");
+
+insert into venda (cliente, data_venda)
+values ("Samuel", "2022-09-13 15:49:03");
+
+insert into venda (cliente, data_venda)
+values ("Felipe", "2022-09-14 09:24:55");
+
+insert into venda (cliente, data_venda)
+values ("Higor", "2022-09-15 11:30:12");
---
+++ triggers.sql
@@ -0,0 +1,17 @@
+-- TRIGGER PARA ATUALIZAR O ESTOQUE
+DELIMITER //
+create trigger tgr_venda_item_insert after insert on venda_item
+for each row
+begin
+	update produto set estoque = estoque - new.quantidade
+	where id = new.id_produto;
+end; //
+
+-- TRIGGER PARA DELETAR O ITEM
+DELIMITER //
+create trigger tgr_venda_item_delete after delete on venda_item
+for each row
+begin
+	update produto set estoque = estoque + old.quantidade
+where id = old.id_produto;
+end; //
